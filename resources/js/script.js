b734d22196587f032fa1dda0cdaa3889312c8729
sadick-yakubu <sadickyakubu1@gmail.com>
2023-02-02 17:13:54 +0000

    add generate button to the site

--- resources/js/script.js
+++ resources/js/script.js
@@ -16,11 +16,16 @@ searchBox.addEventListener("click", ()=>{
 let navLinks = document.querySelector(".nav-links");
 let menuOpenBtn = document.querySelector(".navbar .bx-menu");
 let menuCloseBtn = document.querySelector(".nav-links .bx-x");
+let button = document.querySelector(".btn");
+
 menuOpenBtn.onclick = function() {
 navLinks.style.left = "0";
+button.style.marginRight="-190px";
 }
 menuCloseBtn.onclick = function() {
 navLinks.style.left = "-100%";
+button.style.display = "inline-block";
+button.style.marginRight="-50px";
 }
 // sidebar submenu open close js code
 let arrow = document.querySelector(".arrow");
