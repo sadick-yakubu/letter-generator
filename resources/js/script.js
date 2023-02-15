53f142daaca100daa62c0ba1086be35fd926d438
sadick-yakubu <sadickyakubu1@gmail.com>
2023-02-15 16:24:57 +0000

    add form that will be used to generate the letter

--- resources/js/script.js
+++ resources/js/script.js
@@ -32,3 +32,48 @@ let arrow = document.querySelector(".arrow");
 arrow.onclick = function() {
  navLinks.classList.toggle("show");
 }
+// Letter Generator Form
+const nextButton = document.querySelector(".btn-next");
+const prevButton = document.querySelector(".btn-prev");
+const steps = document.querySelectorAll(".form-step");
+const form_steps = document.querySelectorAll(".step");
+let active = 1;
+nextButton.addEventListener("click",() => {
+  active++;
+  if (active > steps.length){
+    active = steps.length;
+  }
+  updateProgress();
+})
+prevButton.addEventListener("click",() => {
+  active--;
+  if (active < 1){
+    active = 1;
+  }
+  updateProgress();
+})
+
+const updateProgress =()=>{
+  console.log("steps.length =>" + steps.length);
+  console.log("active =>" + active);
+  //toggle .active class for each list item
+  steps.forEach((step, i)=>{
+    if (i==(active-1)){
+      step.classList.add("active");
+      form_steps[i].classList.add("active");
+      console.log("i=>" + i);
+    }else{
+      step.classList.remove("active");
+      form_steps[i].classList.remove("active");
+    }
+  });
+  //enable or disable prev and next buttons
+  if (active===1){
+    prevButton.disabled=true;
+  }else if (active===steps.length){
+    nextButton.disabled=true;
+  }else{
+    prevButton.disabled=false;
+    nextButton.disabled=false;
+  }
+}
